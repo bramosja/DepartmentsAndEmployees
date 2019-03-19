157564f5be50bfcea754b2d9eba3c85a01fef319
unknown <bramosjaneway@gmail.com>
2019-03-19 14:32:05 -0500

    Completed exercises

---
+++ DepartmentAndEmployees/Models/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DepartmentAndEmployees.Models
+{
+        // C# representation of the Department table
+        public class Department
+        {
+            public int Id { get; set; }
+            public string DeptName { get; set; }
+        }
+}
